4aa4eb13ec0dc0083846f46ee83353081dcaaec7
Antonis Zalonis <antrampa@gmail.com>
2023-03-30 23:19:35 +0200

    MixChatClient initial version

---
+++ MixChatClient/src/main/java/MixChatClient.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class MixChatClient {
+
+    public static void main(String[] args) {
+        Client app;
+
+        if(args.length == 0) {
+            app = new Client("127.0.0.1");
+        } else {
+            app = new Client(args[0]);
+        }
+
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app.runClient();
+    }
+}
