658e06c709bb714dc1767a88bb0eca94501f0364
Antonis Zalonis <antrampa@gmail.com>
2023-04-03 19:56:24 +0200

    Changed package

---
+++ MixChatServer/src/main/java/com/mixzalsoftware/MixChatServer.java
@@ -0,0 +1,13 @@
+package com.mixzalsoftware;
+
+import javax.swing.*;
+
+public class MixChatServer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello MixChatServer Here!");
+        Server app = new Server();
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app.runServer();
+    }
+}
