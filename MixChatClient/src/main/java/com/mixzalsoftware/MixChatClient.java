70df381a74147026fd9ad5cd28ee5504a0f9c8bb
Antonis Zalonis <antrampa@gmail.com>
2023-04-03 19:55:53 +0200

    Changed package

---
+++ MixChatClient/src/main/java/com/mixzalsoftware/MixChatClient.java
@@ -0,0 +1,21 @@
+package com.mixzalsoftware;
+
+import com.mixzalsoftware.Client;
+
+import javax.swing.*;
+
+public class MixChatClient {
+
+    public static void main(String[] args) {
+        Client app;
+
+        if(args.length == 0) {
+            app = new Client("127.0.0.1");
+        } else {
+            app = new Client(args[0]);
+        }
+
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app.runClient();
+    }
+}
